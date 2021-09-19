9ec2960e4eb592f217b1914523df98ef6fb98d89
Hossein Ali Asgari <husainjan@gmail.com>
2021-09-19 18:48:22 +0430

    commit

--- product-manage-app/src/components/ProductList/ProductList.jsx
+++ product-manage-app/src/components/ProductList/ProductList.jsx
@@ -39,6 +39,56 @@ const ProductList = () => {
     });
   };
 
+  const renderProducts = () => {
+    let renderedElements = <p>محصولی موجود نمی باشد</p>;
+    console.log(products.length, products);
+    if (products && products.length > 0) {
+      renderedElements = (
+        <>
+          {btnEditSelected && (
+            <Product product={product} setProduct={setProduct} />
+          )}
+          <section className="productsContainer">
+            <div className="productContainer productHeaderContainer bolded">
+              <p className="productsHeaderPadding">ردیف</p>
+              <p className="productsHeaderPadding">نام کالا</p>
+              <p className="productsHeaderPadding">گروه کالا</p>
+              <p className="productsHeaderPadding">قیمت(ریال)</p>
+              <p className="productsHeaderPadding">تغییرات</p>
+            </div>
+            {products &&
+              products.map((product, index) => {
+                return (
+                  <div key={product.id} className="productContainer">
+                    <p className="productsPadding centered">{index + 1}</p>
+                    <p className="productsPadding">{product.name}</p>
+                    <p className="productsPadding">
+                      {Object.values(
+                        productsGroup.find((pg) => pg.id === product.groupId)
+                      ).pop()}
+                    </p>
+                    <p className="productsPadding rtol">
+                      {numberFormat(product.price)}
+                    </p>
+                    <div className="productsPadding icons">
+                      <div onClick={() => editHandler(product.id)}>
+                        <FaRegEdit className="edit" />
+                      </div>
+                      <div onClick={() => deleteHandler(product.id)}>
+                        <FaTrashAlt className="delete" />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </section>
+        </>
+      );
+    }
+
+    return renderedElements;
+  };
+
   return (
     <>
       <h3 style={{ marginTop: "30px", marginBottom: "10px" }}>لیست محصولات</h3>
@@ -58,40 +108,7 @@ const ProductList = () => {
           </AddNewProduct>
         )}
       </section>
-      {btnEditSelected && <Product product={product} setProduct={setProduct} />}
-      <section className="productsContainer">
-        <div className="productContainer productHeaderContainer bolded">
-          <p className="productsHeaderPadding">ردیف</p>
-          <p className="productsHeaderPadding">نام کالا</p>
-          <p className="productsHeaderPadding">گروه کالا</p>
-          <p className="productsHeaderPadding">قیمت(ریال)</p>
-          <p className="productsHeaderPadding">تغییرات</p>
-        </div>
-        {products.map((product, index) => {
-          return (
-            <div key={product.id} className="productContainer">
-              <p className="productsPadding centered">{index + 1}</p>
-              <p className="productsPadding">{product.name}</p>
-              <p className="productsPadding">
-                {Object.values(
-                  productsGroup.find((pg) => pg.id === product.groupId)
-                ).pop()}
-              </p>
-              <p className="productsPadding rtol">
-                {numberFormat(product.price)}
-              </p>
-              <div className="productsPadding icons">
-                <div onClick={() => editHandler(product.id)}>
-                  <FaRegEdit className="edit" />
-                </div>
-                <div onClick={() => deleteHandler(product.id)}>
-                  <FaTrashAlt className="delete" />
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </section>
+      {renderProducts()}
     </>
   );
 };
--- product-manage-app/src/components/Providers/ProductsProvider.jsx
+++ product-manage-app/src/components/Providers/ProductsProvider.jsx
@@ -1,18 +1,19 @@
 import { createContext, useContext, useReducer } from "react";
-const productsInitialState = [
-  {
-    id: 1,
-    name: "فشارسنج بی ول B.Well",
-    price: 5000000,
-    groupId: 2,
-  },
-  {
-    id: 2,
-    name: "ماست پرچرب کاله 500 گرمی",
-    price: 120000,
-    groupId: 1,
-  },
-];
+// const productsInitialState = [
+//   {
+//     id: 1,
+//     name: "فشارسنج بی ول B.Well",
+//     price: 5000000,
+//     groupId: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "ماست پرچرب کاله 500 گرمی",
+//     price: 120000,
+//     groupId: 1,
+//   },
+// ];
+const productsInitialState = [];
 
 const ProductContext = createContext();
 const ProductContextDispatcher = createContext();
@@ -20,7 +21,10 @@ const ProductContextDispatcher = createContext();
 const reducer = (state, action) => {
   switch (action.type) {
     case "add":
-      const lastId = state[state.length - 1].id;
+      let lastId = 0;
+      if (state && state.length > 0) {
+        lastId = state[state.length - 1].id;
+      }
       const newProduct = { ...action.value, id: lastId + 1 };
       let newState = [...state];
       newState.push(newProduct);
